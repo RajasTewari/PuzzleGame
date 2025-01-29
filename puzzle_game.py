f8959fb2ea6fd16b4f7363c38d20f5d225c0a462
RajasTewari <rajastewari@gmail.com>
2025-01-28 20:51:50 -0500

    i have no idea why its not working

--- puzzle_game.py
+++ puzzle_game.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
-# importing grid_maker function
+from PIL import Image, ImageTk
+from tkinter import PhotoImage
 from piece_maker import nine_piecer
 
 # create main class for the game itself
@@ -12,16 +13,44 @@ class PuzzleGame:
         self.window.title(title)
         self.window.geometry(f"{width}x{height}")
 
+        # cutting up the image into 9 pieces
+        self.pieces = nine_piecer("bird.jpg")
+
+        # removing one piece
+        blank_piece = None
+        random_index = random.randint(0, 8)
+        self.pieces[random_index] = blank_piece
+
+        # randomize order by shuffling
+        random.shuffle(self.pieces)
+
         # creating buttons in which we will put our pieces
+        index = 0
+        self.buttons = []
+        self.photos = []
         for i in range(0, 600, 200):
             for j in range(0, 600, 200):
-                # very confused here
-                button = tk.Button(random.choice(nine_piecer("bird.jpg")))
-                button.place(x=i, y=j)
+                if index < len(self.pieces):
 
-    # interaction with buttons
-    def button_click(self):
-        print("no clue")
+                    piece = self.pieces[index]
+
+                    if piece is None:
+                        blank_img = Image.new("RGBA", (200, 200), (0, 0, 0, 0))
+                        photo = ImageTk.PhotoImage(blank_img)
+                    else:
+                        photo = ImageTk.PhotoImage(piece)
+
+                    # create button with the image
+                    button = tk.Button(self.window, image=photo)
+                    button.image = photo
+                    button.place(x=i, y=j)
+
+                    # store button
+                    self.buttons.append(button)
+
+                    index += 1
+
+    # interaction with buttons add later
 
     # function to run the game
     def run(self):
