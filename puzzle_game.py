6beeeb099201d6c76d4da5b1ac673471ac3d3b9a
RajasTewari <rajastewari@gmail.com>
2025-01-28 20:08:05 -0500

    structure of the window shaping up nicely

--- puzzle_game.py
+++ puzzle_game.py
@@ -1,10 +1,32 @@
-import tkinter
+import tkinter as tk
+import random
 # importing grid_maker function
 from piece_maker import nine_piecer
 
+# create main class for the game itself
 class PuzzleGame:
-    def game_window(self):
-        window = tkinter.Tk()
-        window.title = "Puzzle Game"
-        nine_piecer("bird.jpg")
-        window.mainloop()
+    # function to create the window
+    def __init__(self, title, width, height):
+        # don't fully understand the init and self stuff
+        self.window = tk.Tk()
+        self.window.title(title)
+        self.window.geometry(f"{width}x{height}")
+
+        # creating buttons in which we will put our pieces
+        for i in range(0, 600, 200):
+            for j in range(0, 600, 200):
+                # very confused here
+                button = tk.Button(random.choice(nine_piecer("bird.jpg")))
+                button.place(x=i, y=j)
+
+    # interaction with buttons
+    def button_click(self):
+        print("no clue")
+
+    # function to run the game
+    def run(self):
+        self.window.mainloop()
+
+# creating instance of the game
+game = PuzzleGame("Puzzle Game!", 600, 600)
+game.run()
